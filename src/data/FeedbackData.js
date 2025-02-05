ee433c209295f857f643701198ac5bde49421653
Sokorinjo <vsokorac65@gmail.com>
2025-02-05 12:44:35 +0100

    Fixed data

--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -2,17 +2,12 @@ export default [
     {
         id: 1,
         rating: 10,
-        text: 'Vjeruj pobro sve u meni skoci, kad me njen pogledaju oci'
+        text: 'This app is amazing. Good job!'
     },
     {
         id: 2,
-        rating: 8,
-        text: 'Sta se ono tamo cuje, ko to peva u ravnici, to su majko hrabri borci, to su knindze krajisnici'
+        rating: 6,
+        text: "It's good but it needs work. Work on it more."
     },
-    {
-        id: 3,
-        rating: 2,
-        text: 'Nestace bol u grudima, vracam se svojim ljudima, bozijom sam rukom vodjen, umrecu gde sam rodjen'
-    }
 
 ]
